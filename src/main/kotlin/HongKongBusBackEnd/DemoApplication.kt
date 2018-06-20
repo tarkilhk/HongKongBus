486a047454a304bf00b3e38890e68f1539deba43
tarkil <tarkil@gmail.com>
2018-06-20 11:40:51 +0800

    Reorganise code

--- src/main/kotlin/HongKongBusBackEnd/DemoApplication.kt
+++ src/main/kotlin/HongKongBusBackEnd/DemoApplication.kt
@@ -1,4 +1,4 @@
-package com.tarkil.hongkongbus
+package HongKongBusBackEnd
 
 import khttp.responses.Response
 import khttp.structures.cookie.CookieJar
--- src/test/kotlin/HongKongBusBackEnd/DemoApplicationTests.kt
+++ src/test/kotlin/HongKongBusBackEnd/DemoApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.tarkil.hongkongbus
+package HongKongBusBackEnd
 
 import org.junit.Test
 import org.junit.runner.RunWith
