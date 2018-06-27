eae75bb3ad02852f4cf92242e28c30cab296b7a9
tarkil <tarkil@gmail.com>
2018-06-27 23:47:36 +0800

    Remove private modifier on Distance, else it's not added to result json

--- src/main/kotlin/HongKongBusBackEnd/domain/BusStopTime.kt
+++ src/main/kotlin/HongKongBusBackEnd/domain/BusStopTime.kt
@@ -1,7 +1,7 @@
 package HongKongBusBackEnd.domain
 
 data class BusStopTime(val busNumber: Int, val arrivalTime: String, private val rawDistance: String){
-    private val distance = rawDistance.replace("Distance: ", "")
+    val distance = rawDistance.replace("Distance: ", "")
 
     override fun toString(): String = "Bus $busNumber @ $distance - $arrivalTime"
 }
