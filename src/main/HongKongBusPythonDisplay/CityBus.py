a4ca97ca05a992a461c7041a562aff42775a43fa
tarkil <tarkil@gmail.com>
2018-11-03 14:28:13 +0800

    Cleaned API to use correct verbs General clean and refactor of the client/server workflow Adapted CityBus PI client to new workflow

--- src/main/HongKongBusPythonDisplay/CityBus.py
+++ src/main/HongKongBusPythonDisplay/CityBus.py
@@ -32,8 +32,8 @@ DarkWhite = (100, 100, 100)
 PreciousCyan = (102, 245, 173)
 
 # Root URL
-# rootUrl = "https://hong-kong-bus.herokuapp.com"
-rootUrl = "http://127.0.0.1:5000"
+rootUrl = "https://hong-kong-bus.herokuapp.com"
+# rootUrl = "http://127.0.0.1:5000"
 
 # Global variable to be used between the 2 threads
 NextArrivalTimes = []
